46ab0ee7406b0e3272a6e02ee5f0ee27bdafe88e
Marcos A C de Paula <cardepaula@gmail.com>
2019-05-25 23:11:01 -0300

    fix(sh): Fix execute.sh venv

--- source/execute.sh
+++ source/execute.sh
@@ -13,22 +13,35 @@ else
     then
         exit
     else
-        echo "Atualizando os repositorios"
+        echo "_________Atualizando os repositorios_________"
         sudo apt-get update || sudo pacman -Sy
-        echo "Instalando o pip"
-        sudo apt install python3-pip || sudo apt install python-pip || sudo pacman -S python-pip
-        echo "Atualizando o pip"
-        python3 -m pip install --upgrade pip
-        echo "Instalando o virtual env"
-        sudo pip3 install virtualenv
-        echo "Criando o virtual env"
-        python3 -m venv "$DIR_RELATIVO/env"
-        echo "Ativando o virtual env"
-        source "$DIR_RELATIVO/env/bin/activate" || . "$DIR_RELATIVO/env/bin/activate"
-        echo "Instalando o Lark"
-        pip install lark-parser
-        echo "\n\n\n"
+        
+		echo "_________Instalando o pip_________"
+        sudo apt install python3-pip python3-venv || sudo pacman -S python-pip
+
+        echo "_________Instalando o virtual env_________"
+        if sudo pip install virtualenv
+		then
+			echo "_________Criando o virtual env_________"
+			python3 -m venv "$DIR_RELATIVO/env"
+
+			echo "_________Ativando o virtual env_________"
+			if source "$DIR_RELATIVO/env/bin/activate" || . "$DIR_RELATIVO/env/bin/activate"
+			then
+				echo "_________Instalando o Lark_________"
+				pip install lark-parser
+				echo "\n\n\n"
+			else
+				echo "_________Error ao ativar o virtual env_________"
+				exit 1
+			fi
+		else
+			echo "_________Error ao instalar o virtual env_________"
+			exit 1
+		fi
+		clear
     fi
 fi
 
 python3 "$DIR_RELATIVO/mel.py"
+
